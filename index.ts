eb7d5ab0847a0fc3332b019c17777e3dd921d89e
raiden <nadirkichou@hotmail.fr>
2023-10-16 23:43:02 +0200

    :construction: fixed error

--- index.ts
+++
@@ -1,8 +0,0 @@
-import UltimateEntityDecorator from "./src/decorators/ultimate-entity";
-import UltimateEntityFieldDecorator from "./src/decorators/ultimate-entity-field";
-
-export { UltimateEntityDecorator, UltimateEntityFieldDecorator };
-
-export { UltimateEntity } from "./src/types/ultimate-entity";
-export { UltimateEntityField } from "./src/types/ultimate-entity-field";
-export { UltimateEntityFieldTypes } from "./src/types/ultimate-entity-field-types";
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import UltimateEntity from "./decorators/ultimate-entity";
+import UltimateEntityField from "./decorators/ultimate-entity-field";
+
+export { UltimateEntity, UltimateEntityField };
+
+// export { UltimateEntity } from "./types/ultimate-entity";
+// export { UltimateEntityField } from "./types/ultimate-entity-field";
+export { UltimateEntityFieldTypes } from "./types/ultimate-entity-field-types";
